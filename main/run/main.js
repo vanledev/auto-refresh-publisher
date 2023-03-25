692ffdf09bf0c773cc53b64403209ec94755313a
moe <you@example.com>
2023-03-25 20:09:29 +0700

    update

--- main/run/main.js
+++ main/run/main.js
@@ -20,9 +20,34 @@ browser = await cookieFn.importCookies(browser, cookiesArr);
 let page = await browser.newPage();
 page.setDefaultNavigationTimeout(120000);
 await page.goto(process.env.URL, { waitUntil: "load" });
-
 const buttonToClick = await page.waitForSelector(".articles-actions > button");
+await page.addStyleTag({ path: "./style.css" });
+await page.evaluate(() => {
+  const body = document.querySelector("body");
 
-setInterval(() => {
-  buttonToClick.click();
-}, 1000);
+  const extensionAlert = document.createElement("section");
+  extensionAlert.innerHTML = `<div class="extension-alert">
+<p class="extension-alert-text">Tự động Click "Làm Mới" đang: Bật</p>
+<p class="extension-alert-count-wrapper">Đã tự động Click: <span class="extension-alert-count">0</span> lần </p>
+</div>`;
+  body.appendChild(extensionAlert);
+  const countNode = document.querySelector(".extension-alert-count");
+
+  let count = 0;
+  function autoClickRefresh() {
+    const buttonToClick = document.querySelector(".articles-actions > button");
+    buttonToClick.click();
+    count++;
+
+    countNode.innerText = count;
+    console.log(countNode.innerText, count);
+  }
+  autoClickRefresh();
+  interval = setInterval(autoClickRefresh, 600000);
+});
+
+// const buttonToClick = await page.waitForSelector(".articles-actions > button");
+
+// setInterval(() => {
+//   buttonToClick.click();
+// }, 600000);
